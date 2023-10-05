82cae9dfef8cc125c856ebcb2fe1f5282881063b
Flora-Salamandre <fstrugala@iraiser.eu>
2023-10-05 20:41:05 +0200

    [ADD] The movies' titles on posters and clickable titles.

--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,7 +1,21 @@
 import { MovieThumbnail } from "../types";
+import "./Thumbnail.scss";
 
 export function Thumbnail({ thumbnail }: { thumbnail: MovieThumbnail }) {
+    function onClick() {
+        console.log(thumbnail.title)
+    }
+    
     return (
-        <article>{ thumbnail.title }</article>
+        <article 
+            onClick={onClick}
+            className="thumbnail"
+            style={{ backgroundImage: `url(${thumbnail.imageUrl})` }}
+        >
+            <div className="dark-shade">
+                { thumbnail.title.toUpperCase() }
+            </div>
+        </article>
+        
     )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,14 @@
 import { useLoaderData } from "react-router-dom";
 import { MovieThumbnail } from "../../types";
 import { Thumbnail } from "../../components";
+import "./Home.scss"
 
 export function Home() {
     const thumbnails = useLoaderData() as MovieThumbnail[];
 
     return (
-        <>
-            { thumbnails.map(thumbnail => <Thumbnail thumbnail={thumbnail} />) }
-        </>
+        <main className="home">
+            { thumbnails.map(thumbnail => <Thumbnail key={thumbnail.id} thumbnail={thumbnail} />) }
+        </main>
     );
 }
